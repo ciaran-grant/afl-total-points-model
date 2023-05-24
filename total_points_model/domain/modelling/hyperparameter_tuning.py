8fbdf2ed9a016fe8e03f2b33e8700719fb92d86c
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-05-24 14:06:58 +1000

    Newer modelling approach and code added

--- total_points_model/domain/modelling/hyperparameter_tuning.py
+++ total_points_model/domain/modelling/hyperparameter_tuning.py
@@ -5,6 +5,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 from total_points_model.domain.modelling.optuna_xgb_param_grid import OptunaXGBParamGrid
+from total_points_model.domain.contracts.modelling_data_contract import ModellingDataContract
 
 class HyperparameterTuner:
     
@@ -96,19 +97,21 @@ class XGBHyperparameterTuner(HyperparameterTuner, OptunaXGBParamGrid):
         return self.study.best_params
             
              
-class XGBYearHyperparameterTuner(HyperparameterTuner, OptunaXGBParamGrid): 
+class XGBYearHyperparameterTuner(HyperparameterTuner): 
         
-    def __init__(self, training_data, response) -> None:
+    def __init__(self, training_data, response, optuna_grid, monotonicity_constraints):
         super().__init__(training_data, response)
+        self.optuna_grid = optuna_grid
+        self.monotonicity_constraints = monotonicity_constraints
     
     def define_year_folds(self):
     
         years_list = list(self.training_data['Year'].unique())
         test_years = years_list[1:]
-
+        
         year_folds_dict = {}
         for year in test_years:
-            (train_fold, test_fold) = self.training_data[self.training_data['Year'] < year], self.training_data[self.training_data['Year'] == year]
+            (train_fold, test_fold) = self.training_data.loc[self.training_data['Year'] < year].drop(columns = ['Year']), self.training_data[self.training_data['Year'] == year].drop(columns = ['Year'])
             year_folds_dict[year] = (train_fold.index, test_fold.index)
         
         return year_folds_dict
@@ -122,56 +125,61 @@ class XGBYearHyperparameterTuner(HyperparameterTuner, OptunaXGBParamGrid):
         for year, fold in year_folds_dict.items():
 
             train_x, valid_x, train_y, valid_y = self.training_data.iloc[fold[0]], self.training_data.iloc[fold[1]], self.response.iloc[fold[0]], self.response.iloc[fold[1]]
-            dtrain = xgb.DMatrix(train_x, label=train_y)
-            dvalid = xgb.DMatrix(valid_x, label=valid_y)
+            
+            train_x_features = train_x.drop(columns = [ModellingDataContract.ID_COL])
+            valid_x_features = valid_x.drop(columns = [ModellingDataContract.ID_COL])
+            
+            dtrain = xgb.DMatrix(train_x_features, label=train_y)
+            dvalid = xgb.DMatrix(valid_x_features, label=valid_y)
 
             param = {
-                "verbosity": self.verbosity,
-                'objective': self.error,
+                "verbosity": self.optuna_grid.verbosity,
+                'objective': self.optuna_grid.error,
                 # maximum depth of the tree, signifies complexity of the tree.
                 "max_depth" : trial.suggest_int("max_depth",
-                                                self.max_depth_min,
-                                                self.max_depth_max,
-                                                step=self.max_depth_step),
+                                                self.optuna_grid.max_depth_min,
+                                                self.optuna_grid.max_depth_max,
+                                                step=self.optuna_grid.max_depth_step),
                 # minimum child weight, larger the term more conservative the tree.
                 "min_child_weight" : trial.suggest_int("min_child_weight", 
-                                                    self.min_child_weight_min,
-                                                    self.min_child_weight_max,
-                                                    step=self.min_child_weight_step),
+                                                    self.optuna_grid.min_child_weight_min,
+                                                    self.optuna_grid.min_child_weight_max,
+                                                    step=self.optuna_grid.min_child_weight_step),
                 "eta" : trial.suggest_float("eta",
-                                            self.eta_min, 
-                                            self.eta_max, 
-                                            log=True),
+                                            self.optuna_grid.eta_min, 
+                                            self.optuna_grid.eta_max
+                                            ),
                 # defines how selective algorithm is.
                 "gamma" : trial.suggest_float("gamma", 
-                                            self.gamma_min, 
-                                            self.gamma_max, 
-                                            log=True),
+                                            self.optuna_grid.gamma_min, 
+                                            self.optuna_grid.gamma_max
+                                            ),
                 # L2 regularization weight.
                 "lambda": trial.suggest_float("lambda",
-                                            self.lambda_min,
-                                            self.lambda_max, 
-                                            log=True),
+                                            self.optuna_grid.lambda_min,
+                                            self.optuna_grid.lambda_max
+                                            ),
                 # L1 regularization weight.
                 "alpha": trial.suggest_float("alpha", 
-                                            self.alpha_min,
-                                            self.alpha_max,
-                                            log=True),
+                                            self.optuna_grid.alpha_min,
+                                            self.optuna_grid.alpha_max
+                                            ),
                 # sampling ratio for training data.
                 "subsample": trial.suggest_float("subsample", 
-                                                self.subsample_min, 
-                                                self.subsample_max),
+                                                self.optuna_grid.subsample_min, 
+                                                self.optuna_grid.subsample_max),
                 # sampling according to each tree.
                 "colsample_bytree": trial.suggest_float("colsample_bytree",
-                                                        self.colsample_bytree_min, 
-                                                        self.colsample_bytree_max),
-            }        
+                                                        self.optuna_grid.colsample_bytree_min, 
+                                                        self.optuna_grid.colsample_bytree_max),
+            }  
+            param['monotone_constraints'] = self.monotonicity_constraints
 
             bst = xgb.train(param,
                             dtrain,
-                            num_boost_round = self.num_rounds,
+                            num_boost_round = self.optuna_grid.num_rounds,
                             evals = [(dtrain, "train"), (dvalid, "valid")],
-                            early_stopping_rounds = self.early_stopping_rounds)
+                            early_stopping_rounds = self.optuna_grid.early_stopping_rounds)
             preds = bst.predict(dvalid)
         
             rmse = mean_squared_error(preds, valid_y, squared=False)
@@ -187,7 +195,7 @@ class XGBYearHyperparameterTuner(HyperparameterTuner, OptunaXGBParamGrid):
     def tune_hyperparameters(self):
     
         self.study = optuna.create_study(pruner = optuna.pruners.MedianPruner(), direction='minimize')
-        self.study.optimize(self.objective, n_trials=self.trials)
+        self.study.optimize(self.objective, n_trials=self.optuna_grid.trials)
         
         print("Number of finished trials: ", len(self.study.trials))
         print("Best trial:")
