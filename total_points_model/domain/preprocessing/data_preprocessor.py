87eba19af235a87dd6b1bfed13991434b64b6c69
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-05-24 14:00:43 +1000

    Updating preprocessing functions and logic

--- total_points_model/domain/preprocessing/data_preprocessor.py
+++ total_points_model/domain/preprocessing/data_preprocessor.py
@@ -1,4 +1,5 @@
-from total_points_model.domain.preprocessing.preprocessing_functions import score_col_splitter, create_team_rolling_averages, rename_team_stats
+from total_points_model.domain.preprocessing.preprocessing_functions import score_col_splitter, get_team_rolling_averages, rename_rolling_columns, merge_rolling_data
+from total_points_model.domain.preprocessing.elo_calculator import calculate_elo_ratings, convert_elo_dict_to_dataframe, merge_elo_ratings
 from total_points_model.domain.contracts.modelling_data_contract import ModellingDataContract
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -9,94 +10,128 @@ from total_points_model.domain.contracts.mappings import Mappings
 
 class DataPreprocessor(BaseEstimator, TransformerMixin):
     
-    def __init__(self, mapping):
-        self.mapping = mapping
+    def __init__(self, Mappings, rolling_dict):
+        self.Mappings = Mappings
+        self.rolling_dict = rolling_dict
         self.ModellingDataContract = ModellingDataContract
+        
+    def create_team_rolling_average_features(self, X, column, rolling_window, weights):
+    
+        team_data_list = []
+        for team in self.ModellingDataContract.team_list:
+            team_data = get_team_rolling_averages(X, team, column, rolling_window, weights)
+            team_data_list.append(team_data)
+        rolling_data = pd.concat(team_data_list, axis=0).sort_index()
+
+        home_rolling_data = rename_rolling_columns(rolling_data, "Home")
+        away_rolling_data = rename_rolling_columns(rolling_data, "Away")
+
+        X = merge_rolling_data(X, home_rolling_data, away_rolling_data)
+        
+        return X
+    
+    def create_elo_rating_factor(self, X):
+        
+        elos, elo_dict, elo_probs  = calculate_elo_ratings(X, k_factor=ModellingDataContract.ELO_K_FACTOR)
+        
+        X = merge_elo_ratings(X, elos, elo_probs)
+        
+        return X
         
     def fit(self, X, y=None):
         
         X_copy = X.copy()
         
+        # Feature Engineering
         X_copy['Date'] = pd.to_datetime(X_copy['Date'])
-        
-        X_copy['random5'] = np.random.randint(1, 6, X.shape[0])
-        
         X_copy['Round'] = X_copy['Round_ID'].apply(lambda x: x[-2:])
+        X_copy['Finals'] = np.where(X_copy['Round_ID'].str.contains('F'), True, False)
+        X_copy['Primary_Home'] = np.where(X_copy['Home_Ground'] == "Primary Home", True, False)
+        
+        # Feature Grouping and Mappings
+        X_copy['Kicking_Weather'] = X_copy['Weather_Description'].replace(self.Mappings.new_feature_mappings[("Weather_Description", "Kicking_Weather")])
+        X_copy['State'] = X_copy['City'].replace(self.Mappings.new_feature_mappings[('City', 'State')])
+        X_copy['Victoria'] = X_copy['City'].replace(self.Mappings.new_feature_mappings[('City', 'Victoria')])
+        X_copy['Roof'] = X_copy['Venue'].replace(self.Mappings.new_feature_mappings[('Venue', 'Roof')])
 
-        X_copy = X_copy.replace(self.mapping)
+        X_copy['Home_Team_State'] = X_copy['Home_Team'].replace(self.Mappings.meta_mappings[('Team', 'State')])
+        X_copy['Away_Team_State'] = X_copy['Away_Team'].replace(self.Mappings.meta_mappings[('Team', 'State')])
+        X_copy['Home_Team_Within_State'] = np.where(X_copy['State'] == X_copy['Home_Team_State'], True, False)
+        X_copy['Away_Team_Within_State'] = np.where(X_copy['State'] == X_copy['Away_Team_State'], True, False)
 
+        # Apply self mappings
+        X_copy = X_copy.replace(self.Mappings.transformation_mappings)
+
+        # Scoring Columns
         X_copy = score_col_splitter(X_copy, "Q4_Score")
-        
-        teams_list = self.ModellingDataContract.team_list
-        team_df_list = []
-        for team in teams_list:
-            X_team = create_team_rolling_averages(X_copy, team)
-            team_df_list.append(X_team)
-            
-        X_team_data = pd.concat(team_df_list).sort_values(by = "Date")
-        
-        X_home = rename_team_stats(X_team_data, "Home")
-        X_copy = pd.merge(X_copy, X_home, how = 'left', on = 'Match_ID')
-        X_away = rename_team_stats(X_team_data, "Away")
-        X_copy = pd.merge(X_copy, X_away, how = 'left', on = 'Match_ID')
+        # Rolling Averages by Team
+        for name, stat in self.rolling_dict.items():
+            X_copy = self.create_team_rolling_average_features(X_copy, column=stat[0], rolling_window=stat[1], weights=stat[2])
 
-        known_cols_list = ['Home_Team', 'Away_Team', 'Venue', 'Round', 'Year', 'City', 'Temperature', 'Weather_Type', 'random5']
-        rolling_cols_list = [x for x in list(X_copy) if "avg" in x]
-        modelling_cols = known_cols_list + rolling_cols_list
+        # ELO Ratings
+        # print(list(X_copy))
+        X_copy = self.create_elo_rating_factor(X_copy)
+        # print(list(X_copy))
 
-        X_copy = X_copy[modelling_cols]
+        self.rolling_cols_list = [x for x in list(X_copy) if "avg" in x]
+        self.modelling_cols = ModellingDataContract.modelling_feature_list + self.rolling_cols_list
+
+        # Keep only modelling columns and ID
+        X_copy = X_copy[self.modelling_cols]
         
         # Fitting to training data
         self.train_set_means = X_copy.mean()
-        self.weather_mode = X_copy['Weather_Type'].mode()[0]
         self.expected_dummy_cols = list(pd.get_dummies(X_copy))
-                
+                        
         return self
     
     def transform(self, X):
         
+        # Feature Engineering
         X['Date'] = pd.to_datetime(X['Date'])
-     
-        X['random5'] = np.random.randint(1, 6, X.shape[0])
-
         X['Round'] = X['Round_ID'].apply(lambda x: x[-2:])
+        X['Finals'] = np.where(X['Round_ID'].str.contains('F'), True, False)
+        X['Primary_Home'] = np.where(X['Home_Ground'] == "Primary Home", True, False)
+        
+        # Feature Grouping and Mappings
+        X['Kicking_Weather'] = X['Weather_Description'].replace(self.Mappings.new_feature_mappings[("Weather_Description", "Kicking_Weather")])
+        X['State'] = X['City'].replace(self.Mappings.new_feature_mappings[('City', 'State')])
+        X['Victoria'] = X['City'].replace(self.Mappings.new_feature_mappings[('City', 'Victoria')])
+        X['Roof'] = X['Venue'].replace(self.Mappings.new_feature_mappings[('Venue', 'Roof')])
 
-        X = X.replace(self.mapping)
+        X['Home_Team_State'] = X['Home_Team'].replace(self.Mappings.meta_mappings[('Team', 'State')])
+        X['Away_Team_State'] = X['Away_Team'].replace(self.Mappings.meta_mappings[('Team', 'State')])
+        X['Home_Team_Within_State'] = np.where(X['State'] == X['Home_Team_State'], True, False)
+        X['Away_Team_Within_State'] = np.where(X['State'] == X['Away_Team_State'], True, False)
 
+        # Apply self mappings
+        X = X.replace(self.Mappings.transformation_mappings)
+
+        # Scoring Columns
         X = score_col_splitter(X, "Q4_Score")
+        # Rolling Averages by Team
+        for name, stat in self.rolling_dict.items():
+            X = self.create_team_rolling_average_features(X, column=stat[0], rolling_window=stat[1], weights=stat[2])
+
+        # ELO Ratings
+        X = self.create_elo_rating_factor(X)
         
-        teams_list = self.ModellingDataContract.team_list
-        team_df_list = []
-        for team in teams_list:
-            X_team = create_team_rolling_averages(X, team)
-            team_df_list.append(X_team)
-            
-        X_team_data = pd.concat(team_df_list).sort_values(by = "Date")
-        
-        X_home = rename_team_stats(X_team_data, "Home")
-        X = pd.merge(X, X_home, how = 'left', on = 'Match_ID')
-        X_away = rename_team_stats(X_team_data, "Away")
-        X = pd.merge(X, X_away, how = 'left', on = 'Match_ID')
+        self.rolling_cols_list = [x for x in list(X) if "avg" in x]
+        self.modelling_cols = ModellingDataContract.modelling_feature_list + self.rolling_cols_list
 
-        known_cols_list = ['Home_Team', 'Away_Team', 'Venue', 'Round', 'Year', 'City', 'Temperature', 'Weather_Type', 'random5']
-        rolling_cols_list = [x for x in list(X) if "avg" in x]
-        modelling_cols = known_cols_list + rolling_cols_list
-
-        X = X.sort_values(by = "Date")
-
-        X = X[modelling_cols]
+        # Keep only modelling columns and ID
+        X = X[[ModellingDataContract.ID_COL] + self.modelling_cols]
         
         # Applying transformations
-        X = X.fillna(self.train_set_means)
-        X['Weather_Type'] = X['Weather_Type'].fillna(self.weather_mode)
-        
-        X = pd.get_dummies(X)
+        X = X.fillna(self.train_set_means)        
+        X_dummies = pd.get_dummies(X[self.modelling_cols])
         for col in list(self.expected_dummy_cols):
-            if col not in list(X):
-                X[col] = 0
+            if col not in list(X_dummies):
+                X_dummies[col] = 0
                 
-        X = X[self.expected_dummy_cols]
-        
+        X_dummies = X_dummies[self.expected_dummy_cols]
+
+        X = pd.concat([X[ModellingDataContract.ID_COL], X_dummies], axis = 1)
 
         return X
     
--- total_points_model/domain/preprocessing/preprocessing_functions.py
+++ total_points_model/domain/preprocessing/preprocessing_functions.py
@@ -25,58 +25,78 @@ def score_col_splitter(X, score_col):
         
     return X
 
-def create_team_rolling_averages(X, team, rolling_window = 2):
+def create_group_rolling_average(X, column, group, rolling_window):
     
-    X_team = X[(X['Home_Team'] == team) | (X['Away_Team'] == team)]
+    return X.groupby(group)[column].apply(lambda x: x.shift().rolling(rolling_window).mean())
+
+def create_rolling_average(X, column, rolling_window):
+    
+    return X[column].rolling(rolling_window).mean().shift()
+
+def create_weighted_group_rolling_average(X, column, group, rolling_window, weights):
+    
+    assert rolling_window == len(weights)
+    
+    return X.groupby(group)[column].apply(lambda x: x.shift().rolling(rolling_window).apply(lambda x: np.sum(weights*x)))
+
+def create_weighted_rolling_average(X, column, rolling_window, weights):
+    
+    assert rolling_window == len(weights)
+    
+    return X[column].rolling(rolling_window).apply(lambda x: np.sum(weights*x)).shift()
+    
+def create_exp_weighted_group_rolling_average(X, column, group, span_window):
+    
+    return X.groupby(group)[column].apply(lambda x: x.shift().ewm(span = span_window).mean())
+
+def create_exp_weighted_rolling_average(X, column, span_window):
+    
+    return X[column].ewm(span = span_window).mean().shift()
+
+def get_team_rolling_averages(X, team, stat, rolling_window, weights):
+    
+    stat_cols = ["Total_"+stat, "Home_"+stat, "Away_"+stat]
+    
+    X_team = X[(X['Home_Team'] == team )| (X['Away_Team'] == team)]
+    X_team = X_team[["Match_ID", "Home_Team", "Away_Team"] + stat_cols]
     X_team['Team'] = team
-    X_team['Home'] = np.where(X_team['Home_Team'] == team, "Home", 'Away')
-    X_team = X_team.sort_values(by = "Date")
+    X_team['Home_Away'] = np.where(X_team['Home_Team'] == team, 'Home', 'Away')
     
-    quarters_list = ['Q4']
-    stats_list = ['Score', 'Goals', 'Behinds', 'Shots', 'Conversion']
-    quarter_stats_list = [x+"_"+y for x in quarters_list for y in stats_list]
+    X_team['Team_Total_'+stat] = X_team['Total_'+stat]
+    X_team['Team_For_'+stat] = np.where(X_team['Home_Team'] == team, X_team['Home_'+stat], X_team['Away_'+stat])
+    X_team['Team_Against_'+stat] = np.where(X_team['Home_Team'] == team, X_team['Away_'+stat], X_team['Home_'+stat])
     
-    for stat in quarter_stats_list:
-        X_team['Team_Total_'+stat] = X_team['Total_'+stat]
-        X_team['Team_Att_'+stat] = np.where(X_team['Home_Team'] == team, X_team['Home_'+stat], X_team['Away_'+stat])
-        X_team['Team_Def_'+stat] = np.where(X_team['Home_Team'] == team, X_team['Away_'+stat], X_team['Home_'+stat])
+    # X_team['Team_Total_'+stat+"_avg"+str(rolling_window)] = create_rolling_average(X_team, 'Team_Total_'+stat, rolling_window)
+    # X_team['Team_For_'+stat+"_avg"+str(rolling_window)] = create_rolling_average(X_team, 'Team_For_'+stat, rolling_window)
+    # X_team['Team_Against_'+stat+"_avg"+str(rolling_window)] = create_rolling_average(X_team, 'Team_Against_'+stat, rolling_window)
+
+    # X_team['Team_Total_'+stat+"_wavg"+str(rolling_window)] = create_weighted_rolling_average(X_team, 'Team_Total_'+stat, rolling_window, weights)
+    # X_team['Team_For_'+stat+"_wavg"+str(rolling_window)] = create_weighted_rolling_average(X_team, 'Team_For_'+stat, rolling_window, weights)
+    # X_team['Team_Against_'+stat+"_wavg"+str(rolling_window)] = create_weighted_rolling_average(X_team, 'Team_Against_'+stat, rolling_window, weights)
+
+    X_team['Team_Total_'+stat+"_exp_wavg"+str(rolling_window)] = create_exp_weighted_rolling_average(X_team, 'Team_Total_'+stat, rolling_window)
+    # X_team['Team_For_'+stat+"_exp_wavg"+str(rolling_window)] = create_exp_weighted_rolling_average(X_team, 'Team_For_'+stat, rolling_window)
+    # X_team['Team_Against_'+stat+"_exp_wavg"+str(rolling_window)] = create_exp_weighted_rolling_average(X_team, 'Team_Against_'+stat, rolling_window)
+   
+    return X_team
+
+def rename_rolling_columns(X, home_away):
+    
+    rolling_avg_cols = [x for x in list(X) if "avg" in x]
+
+    X_rolling = X[X['Home_Away'] == home_away]
+    X_rolling_avg_cols = [x.replace("Team", home_away) for x in rolling_avg_cols]
+    rename_dict = dict(zip(rolling_avg_cols, X_rolling_avg_cols))
+    X_rolling = X_rolling.rename(columns=rename_dict)
+    
+    X_rolling = X_rolling[['Match_ID'] + X_rolling_avg_cols]
         
-    home_cols = [x for x in list(X_team) if "Home_Q" in x]
-    away_cols = [x for x in list(X_team) if "Away_Q" in x]
-    
-    numeric_cols = list(X_team.select_dtypes('number'))
-    rolling_cols = [x for x in numeric_cols if "Team" in x] #+ [x for x in numeric_cols if "Total" in x]
-    
-    rolling_team_data = X_team.groupby('Team')[rolling_cols].rolling(rolling_window).mean()
-    rolling_team_data.columns = [x+"_avg" + str(rolling_window) for x in rolling_cols]
-    rolling_team_data = rolling_team_data.reset_index(level = [0])
-    rolling_team_data = rolling_team_data.drop(columns=['Team'])
-    
-    X_team = pd.merge(X_team, rolling_team_data, how = 'left', left_index=True, right_index=True)
-    
-    return X_team
+    return X_rolling
 
-def rename_team_stats(X, home = "Home", rolling_window = 2):
+def merge_rolling_data(X, X_home, X_away):
     
-    X_team = X[X['Home'] == home]
-    
-    avg_cols = [x for x in list(X_team) if "avg"+str(rolling_window) in x]
-    team_total_cols = [x for x in avg_cols if 'Team_Total' in x]
-    team_att_cols = [x for x in avg_cols if 'Team_Att' in x]
-    team_def_cols = [x for x in avg_cols if 'Team_Def' in x]
-    
-    total_cols = [x.replace("Team_Total", home+"_Total") for x in team_total_cols]
-    att_cols = [x.replace("Team_Att", home+"_Att") for x in team_att_cols]
-    def_cols = [x.replace("Team_Def", home+"_Def") for x in team_def_cols]
+    X = pd.merge(X, X_home, how = 'left', on = 'Match_ID')
+    X = pd.merge(X, X_away, how = 'left', on = 'Match_ID')
+   
+    return X
 
-    team_total_dict = dict(zip(team_total_cols, total_cols))
-    team_att_dict = dict(zip(team_att_cols, att_cols))
-    team_def_dict = dict(zip(team_def_cols, def_cols))
-    
-    X_team = X_team.rename(columns=team_total_dict)
-    X_team = X_team.rename(columns=team_att_dict)
-    X_team = X_team.rename(columns=team_def_dict)
-    
-    X_team = X_team[['Match_ID']+total_cols+att_cols+def_cols]
-    
-    return X_team
